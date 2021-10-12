b4795a8775dd210bd0fb351b8ccd5b75e0037fe5
LipinVV <vit.lipin@gmail.com>
2021-10-12 15:08:23 +0300

    added feature to change task text and save it in a proper way plus put it in a localstorage

--- src/Tasks/Tasks.js
+++ src/Tasks/Tasks.js
@@ -1,33 +1,46 @@
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {taskManagementAddTask, taskManagementRemoveTask, taskManagementUpdateTask} from "../actions/actions";
 import './tasks.css';
 
 export const Tasks = () => {
     const curState = useSelector(state => state);
     const dispatch = useDispatch();
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState('');
     const [task, setTask] = useState([]);
     const [taskId, setTaskId] = useState(0);
     const [groupTask, setGroundTask] = useState([]);
-    const [newValue, setNewValue] = useState('');
+    const [editingText, setEditingText] = useState("");
+    const [taskToChange, setTaskToChange] = useState([]);
+
+    useEffect(() => {
+        const json = localStorage.getItem("task");
+        const loadedTasks = JSON.parse(json);
+        if (loadedTasks) {
+            setTask(loadedTasks);
+        }
+    }, []);
+
+    useEffect(() => {
+        const json = JSON.stringify(task);
+        localStorage.setItem("task", json);
+    }, [task]);
 
     const handleChanger = (evt) => {
         const {value} = evt.target;
         setValue(value);
     }
 
-
     const editTaskHandleChanger = (evt) => {
         const {value} = evt.target;
-        setNewValue(value);
+        setEditingText(value);
     }
 
     const submitTaskHandler = () => {
         setTaskId(prevState => prevState + 1);
         setTask([
             ...task,
-            {text: value, completed: false, id: taskId}
+            {text: value, completed: false, id: taskId, changed: false}
         ]);
     }
 
@@ -40,19 +53,19 @@ export const Tasks = () => {
             }
             return oneTask;
         }))
+        setTaskToChange(id)
     }
 
     const saveTextTask = (id) => {
         setTask(task.map(oneTask => {
             if (oneTask.id === id) {
                 return {
-                    ...oneTask, text: newValue, changed: false
+                    ...oneTask, text: editingText, changed: false
                 }
             }
             return oneTask;
         }))
     }
-    console.log(task)
     const removeTaskHandler = (id) => {
         setTask(task.filter(issue => issue.id !== id));
     }
@@ -92,11 +105,8 @@ export const Tasks = () => {
         return groupTask;
     }
     console.log(curState)
-    // to think about how to push / transfer somewhere else filtered tasks, to another page maybe
-    // keep tasks in a localstorage
 
-    const [correction, setCorrection] = useState(false);
-    // console.log(correction)
+
     return (
         <div className='tasks'>
             <input value={value} onChange={handleChanger} type='text'/>
@@ -119,58 +129,62 @@ export const Tasks = () => {
                 <option value='uncompleted'>Uncompleted</option>
             </select>
             <div className='tasks__tasks-block'>
-                {groupTask.map((task, index) => {
-                    return (
-                        <div
-                            className={!task.completed ? 'task__single-task' : 'task__single-task task__single-task-completed'}
-                            key={Date.now() + index}>
-                            <div>id: {task.id + 1}</div>
-                            <div>{!task.changed && task.id === index ? task.text : <input value={newValue} type='text' onChange={editTaskHandleChanger}/>}</div>
-                            <button
-                                type='button'
-                                className={!task.completed ? 'tasks__button' : 'tasks__button tasks__button-completed'}
-                                onClick={() => {
-                                    completeHandler(task.id);
-                                    dispatch(taskManagementUpdateTask(task));
-                                }
-                                }>{task.completed ? 'Completed' : 'Complete'}
-                            </button>
-                            <button
-                                type='button'
-                                className='tasks__button tasks__button-remove'
-                                onClick={() => {
-                                    removeTaskHandler(task.id);
-                                    dispatch(taskManagementRemoveTask(task));
-                                }}>Remove task
-                            </button>
-                            <button
-                                type='button'
-                                onClick={() => {
-                                    editTextTask(task.id)
-                                    // console.log(task.id, index)
-                                }}
-                            >
-                                Edit
-                            </button>
-                            <button
-                                type='button'
-                                onClick={() => task.id === index ? setCorrection(false) : null}
-                            >
-                                Discard
-                            </button>
-                            <button
-                                type='button'
-                                onClick={() => {
-                                    saveTextTask(task.id);
-                                    setNewValue('')
-                                }}
-                            >
-                                Save
-                            </button>
+                {groupTask.map((task, index) =>
+                    <div
+                        className={!task.completed ? 'task__single-task' : 'task__single-task task__single-task-completed'}
+                        key={task.id}>
+                        <div>
+                            id: {task.id}
+                            {task.changed  ?
+                                <input
+                                    type='text'
+                                    value={editingText}
+                                    onChange={editTaskHandleChanger}/> : <div>{task.text}</div>
+                            }
                         </div>
-                    )
-                })}
+                        <button
+                            type='button'
+                            className={!task.completed ? 'tasks__button' : 'tasks__button tasks__button-completed'}
+                            onClick={() => {
+                                completeHandler(task.id);
+                                dispatch(taskManagementUpdateTask(task));
+                            }
+                            }>{task.completed ? 'Completed' : 'Complete'}
+                        </button>
+                        <button
+                            type='button'
+                            className='tasks__button tasks__button-remove'
+                            onClick={() => {
+                                removeTaskHandler(task.id);
+                                dispatch(taskManagementRemoveTask(task));
+                            }}>Remove task
+                        </button>
+                        <button
+                            type='button'
+                            onClick={() => {
+                                editTextTask(task.id)
+                            }}
+                        >
+                            Edit
+                        </button>
+                        <button
+                            type='button'
+                        >
+                            Discard
+                        </button>
+                        <button
+                            type='button'
+                            onClick={() => {
+                                saveTextTask(task.id);
+                            }}
+                        >
+                            Save
+                        </button>
+                    </div>
+                )
+                }
             </div>
+
         </div>
     )
 }
